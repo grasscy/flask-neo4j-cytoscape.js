ab28637545d9cce678b050f6f4dbc84627add90a
grasscy <grassc@qq.com>
2019-11-08 07:25:38 +0800

    删除多余文件

--- test/neo4j_ogm.py
+++
@@ -1,7 +0,0 @@
-from py2neo import Graph
-from app import Ccp
-
-##连接neo4j数据库，输入地址、用户名、密码
-graph = Graph('http://182.254.156.112:7474', username='neo4j', password='root1234')
-
-c = Ccp.create(graph)
